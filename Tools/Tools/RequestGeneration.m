a9351bffc27366f0163fcc58681cafe32b0cf78e
项工 <xiangpuhua@126.com>
2020-06-08 09:58:54 +0800

    no message

--- Tools/Tools/RequestGeneration.m
+++ Tools/Tools/RequestGeneration.m
@@ -474,9 +474,9 @@ static NSDictionary *configDictionary;
                     [result appendFormat:@"\t[requestParams addEntriesFromDictionary:iparams];\n"];
                     [result appendFormat:@"\tNSString *baseUrl = [PHRequest baseURL:[NSString stringWithFormat:@\"%%@/%@/%%@\", BASE_URL, %@] extraData:extraData];\n", baseURL, [configDictionary[@"baseurl"] boolValue]?@"baseurl":@"@\"\""];
                     
-                    [result appendFormat:@"\tNSDictionary *parameters = ([PHRequest uploadParams:requestParams extraData:extraData]);\n"];
-                    [result appendFormat:@"\tYLT_Log(@\"%%@ %%@\", baseUrl, extraData);\n"];
                     [result appendFormat:@"\tNSTimeInterval startTime = [[NSDate date] timeIntervalSince1970];\n"];
+                    [result appendFormat:@"\tNSDictionary *parameters = ([PHRequest baseUrl:baseUrl uploadParams:requestParams currentTime:startTime extraData:extraData success:success failure:failure]);\n"];
+                    [result appendFormat:@"//\tYLT_Log(@\"%%@ %%@\", baseUrl, extraData);\n"];
                     
                     NSString *queryString = [self allPramaFromContents:contents withType:TYPE_QUERY fileType:fileType cacheDay:cacheDay];
                     if (queryString.length > 1) {
@@ -497,7 +497,7 @@ static NSDictionary *configDictionary;
                     }
                     [result appendFormat:@"\t\tNSInteger duration = ([[NSDate date] timeIntervalSince1970]-startTime)*1000;\n"];
                     [result appendFormat:@"\t\tid decryptResult = ([PHRequest responseTitle:@\"%@\" result:result baseUrl:baseUrl parameters:requestParams duration:duration extraData:extraData]);\n", [[contents firstObject] stringByReplacingOccurrencesOfString:@"/" withString:@""]];
-                    [result appendString:@"\t\tYLT_Log(@\"%@ %@ %@\", baseUrl, extraData, decryptResult);\n"];
+                    [result appendString:@"//\t\tYLT_Log(@\"%@ %@ %@\", baseUrl, extraData, decryptResult);\n"];
                     
                     [result appendFormat:@"\t\tBaseCollection *res = [BaseCollection mj_objectWithKeyValues:decryptResult];\n"];
                     [result appendString:@"\t\tid data = decryptResult[@\"body\"];\n"];
@@ -506,28 +506,34 @@ static NSDictionary *configDictionary;
                     if (![returnType isEqualToString:@"BaseCollection"] && ![returnType isEqualToString:@"NSDictionary"]) {
                         if (returnIsValueType) {
                             [result appendFormat:@"\t\t\tif ([data isKindOfClass:[NSObject class]]) {\n"];
-                            [result appendFormat:@"\t\t\t\t%@ *info = data;\n", modelname];
-                            [result appendString:@"\t\t\t\tsuccess(task, res, @[info].mutableCopy, result);\n"];
+                            [result appendFormat:@"\t\t\t\t%@ *info = @[data].mutableCopy;\n", modelname];
+                            [result appendString:@"\t\t\t\tsuccess(task, res, info, result);\n"];
+                            [result appendFormat:@"\t\t\t\t[PHRequest responseSessionDataTask:task baseCollection:res data:info sourceData:decryptResult error:nil];\n"];
                         } else {
                             if (returnIsList) {//返回的数据类型是数组
                                 [result appendFormat:@"\t\t\tif ([data isKindOfClass:[NSDictionary class]]) {\n"];
-                                [result appendFormat:@"\t\t\t\t%@ *info = [%@ mj_objectWithKeyValues:data];\n", modelname, modelname];
-                                [result appendString:@"\t\t\t\tsuccess(task, res, @[info].mutableCopy, result);\n"];
+                                [result appendFormat:@"\t\t\t\t%@ *info = @[[%@ mj_objectWithKeyValues:data]].mutableCopy;\n", modelname, modelname];
+                                [result appendString:@"\t\t\t\tsuccess(task, res, info, result);\n"];
+                                [result appendFormat:@"\t\t\t\t[PHRequest responseSessionDataTask:task baseCollection:res data:info sourceData:decryptResult error:nil];\n"];
                                 [result appendString:@"\t\t\t} else if ([data isKindOfClass:[NSArray class]]) {\n"];
                                 [result appendFormat:@"\t\t\t\tNSMutableArray *resultList = [%@ mj_objectArrayWithKeyValuesArray:data];\n", modelname];
                                 [result appendFormat:@"\t\t\t\tsuccess(task, res, resultList, decryptResult);\n"];
+                                [result appendFormat:@"\t\t\t\t[PHRequest responseSessionDataTask:task baseCollection:res data:resultList sourceData:decryptResult error:nil];\n"];
                             } else {
                                 [result appendFormat:@"\t\t\tif ([data isKindOfClass:[NSDictionary class]]) {\n"];
                                 [result appendFormat:@"\t\t\t\t%@ *info = [%@ mj_objectWithKeyValues:data];\n", returnType, returnType];
                                 [result appendString:@"\t\t\t\tsuccess(task, res, info, decryptResult);\n"];
+                                [result appendFormat:@"\t\t\t\t[PHRequest responseSessionDataTask:task baseCollection:res data:info sourceData:decryptResult error:nil];\n"];
                             }
                         }
                         
                         [result appendFormat:@"\t\t\t} else {\n"];
                         [result appendString:@"\t\t\t\tsuccess(task, res, data, decryptResult);\n"];
+                        [result appendFormat:@"\t\t\t\t[PHRequest responseSessionDataTask:task baseCollection:res data:data sourceData:decryptResult error:nil];\n"];
                         [result appendFormat:@"\t\t\t}\n"];
                     } else {
                         [result appendString:@"\t\t\tsuccess(task, res, data, decryptResult);\n"];
+                        [result appendFormat:@"\t\t\t\t[PHRequest responseSessionDataTask:task baseCollection:res data:data sourceData:decryptResult error:nil];\n"];
                     }
                     [result appendFormat:@"\t\t}\n"];
                     [result appendString:@"\t};\n"];
@@ -636,6 +642,7 @@ static NSDictionary *configDictionary;
                     [result appendFormat:@"\t\tif (failure) {\n"];
                     [result appendFormat:@"\t\t\tfailure(task, error);\n"];
                     [result appendFormat:@"\t\t}\n"];
+                    [result appendFormat:@"\t\t[PHRequest responseSessionDataTask:task baseCollection:nil data:nil sourceData:nil error:error];\n"];
                     [result appendString:@"\t}];\n"];
                     [result appendString:@"\treturn op;\n"];
                     [result appendString:@"}\n\n"];
